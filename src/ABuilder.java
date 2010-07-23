e9967e68a5ba2109da01a6d0069e15af369071ae
allometry <steven.jabour@gmail.com>
2010-07-23 07:12:32 -0600

    Debugged building and removing. Adding in butler procedures.

--- src/ABuilder.java
+++ src/ABuilder.java
@@ -13,18 +13,25 @@ import org.rsbot.script.wrappers.RSObject;
 public class ABuilder extends Script {
 	private int rickServantID = 0, maidServantID = 0, cookServantID = 0, butlerServantID = 4241, demonButlerServantID = 0;
 	private int larderSpaceID = 15403, oakLarderID = 13566;
-	private int oakPlankID = 0, oakPlankNoteID = 0, oakLogID = 0, oakLogNoteID = 0;
+	private int oakPlankID = 8778, oakPlankNoteID = 0, oakLogID = 0, oakLogNoteID = 1522;
+	private int objectsInServantInventory = 0;
 	
-	private int buildLarderParentInterface = 394, removeConfirmParentInterface = 228;
-	private int buildOakLarderChildInterface = 11, removeConfirmYesChildInterface = 2;
-	private int unnoteLogsConfirmParentInterface = 230, unnoteLogsConfirmParentChildInterface = 2;
-	private int sendLogsToSawmillParent = 228, sendLogsToSawmillChild = 3;
-	private int holdPlankParentInterface = 241, holdPlankChildInterface = 4;
-	private int buildOakLarderComponent = 1;
+	private Servant demonButlerServant = new Servant(4243, 26);
+	private Servant butlerServant = new Servant(4241, 20);
+	
+	public class Servant {
+		public int id;
+		public int inventorySpace;
+		
+		public Servant(int id, int inventorySpace) {
+			this.id = id;
+			this.inventorySpace = inventorySpace;
+		}
+	}
 	
 	private long timeout = 0;
 	
-	private boolean withdrawFromBank = false; 
+	private boolean isServantHoldingPlanks = false, withdrawFromBank = false; 
 	
 	@Override
 	public boolean onStart(Map<String,String> args) {
@@ -33,47 +40,80 @@ public class ABuilder extends Script {
 	
 	@Override
 	public int loop() {
-		log("Looping...");
-		
-		//Build Oak Larder
-		if(!getInterface(394, 228).isValid()) {
-			RSObject larderSpace = getNearestObjectByID(15403);
-			if(larderSpace != null) {
-				timeout = System.currentTimeMillis() + 5000;
-				while(getInterface(394, 228) == null && System.currentTimeMillis() < timeout) {
-					atObject(larderSpace, "Build");
-					wait(random(1000,2000));
+		try {
+			log("Looping...");
+			//Pay the Servant
+			//Uses same interface as holding items, so we need some logic!
+			//Check if Child 4 contains, "Master, if thou desirest my continued service"
+			//Then click Child 7
+			//Wait for interface 232
+			//If paying from bank, click Child 3
+			//Else click Child 2
+			//Thank the butler, Parent 241, click Child 5
+			
+			//Un-note logs
+			//Parent 230, Child 2
+			
+			//Un-note number entry
+			//Parent 752, Child 5
+			//Send 26
+			
+			//Send to sawmill
+			//Parent 228, Child 3
+			
+			//Check for butler holding no items
+			//Parent 242
+			
+			//Check for butler holding items
+			//Parent 243
+			
+			//Check if we need to utilize the butler
+			if(!isInventoryFull()) {
+				RSNPC servant = getNearestFreeNPCByID(butlerServant.id, demonButlerServant.id);
+				if(servant != null) {
+					//If the butler is holding planks, grab them.
+					if(isServantHoldingPlanks) {
+						atNPC(servant, null);
+						
+					}
 				}
+			}
+			
+			//Build Oak Larder
+			RSObject larderSpace = getNearestObjectByID(15403);
+			if(larderSpace != null && (!getInterface(394).isValid() || getInterface(394, 11).getAbsoluteY() < 30)) {
+				log("Moving to click object");
+				
+				atObject(larderSpace, "Build Larder space");
 				
 				return random(1000, 1500);
-			} else {
-				log("Couldn't find oak larder space...");
+			} else if(getInterface(394).isValid() && getInterface(394, 11).getAbsoluteX() > 30) {
+				RSInterfaceComponent buildOakLarder = new RSInterfaceComponent(getInterface(394), 11, 1);
+				log((buildOakLarder == null) ? "Interface component is null" : "Found interface component");
+				
+				if(buildOakLarder != null)
+					atComponent(buildOakLarder, "Build Oak larder");
 			}
-		} else {
-			log("Interface is valid, skipping logic...");
-		}
-		
-		//Build Oak Larder Interface / Component
-		if(getInterface(394, 228).isValid()) {
-			RSInterfaceComponent buildOakLarder = new RSInterfaceComponent(getInterface(394), 228, 1);
 			
-			if(buildOakLarder != null) {
-				if(buildOakLarder.isValid()) {
-					timeout = System.currentTimeMillis() + 5000;
-					while(buildOakLarder.isValid() && System.currentTimeMillis() < timeout) {
-						atComponent(buildOakLarder, "Build");
-						wait(random(1000,2000));
-					}
-					
-					return random(1000, 1500);
-				}
-			} else {
-				log("Couldn't find build larder interface and component...");
+			//Remove Oak Larder
+			RSObject oakLarder = getNearestObjectByID(oakLarderID);
+			if(oakLarder != null && !getInterface(228).isValid()) {
+				log("Removing larder");
+				
+				atObject(oakLarder, "Remove Larder");
+				
+				return random(1000, 1500);
+			} else if(getInterface(228).isValid()) {
+				RSInterfaceChild confirmInterface = getInterface(228, 2);
+				
+				atInterface(confirmInterface, true);
+				
+				return random(1000, 1500);
 			}
-		} else {
-			log("Build interface isn't valid...");
+		} catch(Exception e) {
+			log("Caught exception: " + e.getMessage());
 		}
-		
+			
 		return 1;
 	}
 	
